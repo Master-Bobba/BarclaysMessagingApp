1753fc21b13361c07d6324ecd9c0119136c97286
Master-Bobba <bayvazov94@gmail.com>
2023-11-14 11:22:50 +0000

    more tests

---
+++ src/test/java/com/barclays/controller/PersonControllerIntegrationTests.java
@@ -0,0 +1,28 @@
+package com.barclays.controller;
+
+import com.barclays.model.Person;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class PersonControllerIntegrationTests {
+    @Autowired
+    PersonController personController;
+
+    @Test
+    public void testGetAllPeople(){
+        List<Person> people = personController.getAllPeople();
+        assertEquals(2, people.size());
+    }
+
+    @Test
+    public void testSinglePerson(){
+        Person person = personController.getPerson(0, "FILTER");
+        assertEquals("Alice", person.getName());
+    }
+}
